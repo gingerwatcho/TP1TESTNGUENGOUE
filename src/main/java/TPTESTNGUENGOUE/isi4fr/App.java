d53f8aada6196afc8e353d5a51fb8974b3d641b0
nguengoue.serena <nguengoue.serena@institutsaintjean.org>
2024-10-17 16:57:08 +0200

    Initial commit

---
+++ src/main/java/TPTESTNGUENGOUE/isi4fr/App.java
@@ -0,0 +1,13 @@
+package TPTESTNGUENGOUE.isi4fr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
